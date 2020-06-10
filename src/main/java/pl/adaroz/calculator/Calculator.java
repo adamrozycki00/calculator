cccddd846dd182ef0c97f5d0fc7c081520f96111
Adam Rozycki <adamrozycki00@gmail.com>
2020-06-10 10:30:31 +0200

    Add calss Calculator

--- src/main/java/pl/adaroz/calculator/Calculator.java
+++ src/main/java/pl/adaroz/calculator/Calculator.java
@@ -2,6 +2,22 @@ package pl.adaroz.calculator;
 
 public class Calculator {
 
-    
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        double a = 6;
+        double b = 9;
+        System.out.printf("%f + %f = %f\n", a, b, calc.add(a, b));
+        a = 4.5;
+        b = 3.1;
+        System.out.printf("%f - %f = %f\n", a, b, calc.subtract(a, b));
+    }
 
 }
